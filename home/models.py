c69d78c4565f636768b3c5739f9fde9bdabe11eb
Sushant <sushantp053@gmail.com>
2023-12-20 18:35:49 +0530

    first commit

---
+++ home/models.py
@@ -0,0 +1,18 @@
+# from django.db import models
+from django.contrib.gis.db import models
+
+
+class Natoshi(models.Model):
+    nid = models.AutoField(primary_key=True)
+    id = models.IntegerField()
+    gutno = models.IntegerField()
+    crop = models.CharField(max_length=100)
+    area = models.FloatField()
+    village = models.CharField(max_length=100)
+    remarks = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    geom = models.MultiPolygonField()
+
+    class Meta:
+        managed = False
+        db_table = 'Natoshi'
